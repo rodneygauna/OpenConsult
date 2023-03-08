18ef9b01f69f2def861ac2d130510830c37cadc9
Rodney Gauna <rodneygauna@gmail.com>
2023-03-07 17:03:31 -0800

    creation of app dictionaries

---
+++ src/dictionaries/dict_general.py
@@ -0,0 +1,33 @@
+'''
+List of all dictionaries used in the application.
+'''
+
+
+# Status Dictionary
+STATUS = [
+    ('Active', 'Active'),
+    ('Inactive', 'Inactive'),
+    ('Pending', 'Pending')
+]
+
+# Name Prefix Dictionary
+NAME_PREFIX = [
+    ('', ''),
+    ('MR', 'Mr.'),
+    ('MRS', 'Mrs.'),
+    ('MS', 'Ms.'),
+    ('DR', 'Doctor'),
+    ('REV', 'Reverend')
+]
+
+# Name Suffix Dictionary
+NAME_SUFFIX = [
+    ('', ''),
+    ('JR', 'Junior'),
+    ('SR', 'Senior'),
+    ('I', 'The First'),
+    ('II', 'The Second'),
+    ('III', 'The Third'),
+    ('IV', 'The Fourth'),
+    ('V', 'The Fifth')
+]
