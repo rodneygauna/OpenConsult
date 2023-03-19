9296193d111179f8bdc202c8478bccc3027020e9
Rodney Gauna <rodneygauna@gmail.com>
2023-03-19 23:32:48 +0000

    can edit a patient's profile

--- src/patients/views.py
+++ src/patients/views.py
@@ -95,3 +95,79 @@ def add_patient(practice_id):
     return render_template('patients/add_patient.html',
                            title='OpenConsult - Add Patient',
                            form=form)
+
+
+# Edit patient
+@ patient_bp.route('/patient/<int:patient_id>/edit', methods=['GET', 'POST'])
+@login_required
+def edit_patient(patient_id):
+    '''Edits a patient's information'''
+
+    form = PatientForm()
+
+    # Queries patient's information
+    patient = Patient.query.get_or_404(patient_id)
+
+    # Get patient information
+    if request.method == 'GET':
+        form.firstname.data = patient.firstname
+        form.middlename.data = patient.middlename
+        form.lastname.data = patient.lastname
+        form.suffixname.data = patient.suffixname
+        form.status.data = patient.status
+        form.date_of_birth.data = patient.date_of_birth
+        form.sex.data = patient.sex
+        form.gender_idenity.data = patient.gender_idenity
+        form.sexual_orientation.data = patient.sexual_orientation
+        form.mobile_number.data = patient.mobile_number
+        form.home_number.data = patient.home_number
+        form.work_number.data = patient.work_number
+        form.street_number.data = patient.street_number
+        form.street_prefix.data = patient.street_prefix
+        form.street_name.data = patient.street_name
+        form.street_type.data = patient.street_type
+        form.street_suffix.data = patient.street_suffix
+        form.apartment_unit_room.data = patient.apartment_unit_room
+        form.po_box_address.data = patient.po_box_address
+        form.city.data = patient.city
+        form.state.data = patient.state
+        form.zipcode.data = patient.zipcode
+
+    # Edits patient's information
+    if form.validate_on_submit():
+        # Check if user clicked cancel
+        if form.cancel.data:
+            return redirect(url_for('patient.view_patient',
+                                    patient_id=patient_id))
+        patient.firstname = form.firstname.data
+        patient.middlename = form.middlename.data
+        patient.lastname = form.lastname.data
+        patient.suffixname = form.suffixname.data
+        patient.status = form.status.data
+        patient.date_of_birth = form.date_of_birth.data
+        patient.sex = form.sex.data
+        patient.gender_idenity = form.gender_idenity.data
+        patient.sexual_orientation = form.sexual_orientation.data
+        patient.mobile_number = form.mobile_number.data
+        patient.home_number = form.home_number.data
+        patient.work_number = form.work_number.data
+        patient.street_number = form.street_number.data
+        patient.street_prefix = form.street_prefix.data
+        patient.street_name = form.street_name.data
+        patient.street_type = form.street_type.data
+        patient.street_suffix = form.street_suffix.data
+        patient.apartment_unit_room = form.apartment_unit_room.data
+        patient.po_box_address = form.po_box_address.data
+        patient.city = form.city.data
+        patient.state = form.state.data
+        patient.zipcode = form.zipcode.data
+        patient.updated_date = datetime.utcnow()
+        patient.updated_by = current_user.id
+        db.session.commit()
+        flash('Patient updated successfully.', 'success')
+        return redirect(url_for('patient.view_patient', patient_id=patient_id))
+
+    return render_template('patients/add_patient.html',
+                           title='OpenConsult - Edit Patient',
+                           form=form,
+                           patient=patient)
