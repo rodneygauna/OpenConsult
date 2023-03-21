5eee57258a07192f2c332710b70a4b92a99b1e88
Rodney Gauna <rodneygauna@gmail.com>
2023-03-21 23:24:54 +0000

    fixed model issue

--- src/models.py
+++ src/models.py
@@ -163,3 +163,32 @@ class Patient(db.Model):
 
     def __repr__(self):
         return f"Patient Name: {self.firstname} {self.lastname}"
+
+
+class Consult(db.Model):
+    '''Consult model'''
+
+    __tablename__ = 'consults'
+
+    # IDs
+    id = db.Column(db.Integer, primary_key=True)
+    patient_id = db.Column(db.Integer, db.ForeignKey('patients.id'))
+    creating_provider_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    assigned_specialist_id = db.Column(
+        db.Integer, db.ForeignKey('users.id'))
+    # Timestamps
+    created_date = db.Column(
+        db.DateTime, nullable=False, default=datetime.utcnow)
+    created_by = db.Column(db.Integer, db.ForeignKey('users.id'))
+    updated_date = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_by = db.Column(db.Integer, db.ForeignKey('users.id'))
+    # Status
+    status = db.Column(db.String(50), default='DRAFT')
+    # Consult Information
+    specialty = db.Column(db.String(255), nullable=False)
+    chief_complaint = db.Column(db.Text, nullable=False)
+    comments_to_specialist = db.Column(db.Text)
+    main_question = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return f"Consult ID: {self.id}"
