c0039efe09521eb1c9e03081e07f8a3b40e02653
Rodney Gauna <rodneygauna@gmail.com>
2023-06-02 15:23:42 +0000

    start of the econsult response form

--- src/econsult/forms.py
+++ src/econsult/forms.py
@@ -7,6 +7,7 @@ eConsult forms for the application.
 from flask_wtf import FlaskForm
 from wtforms import SelectField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
+from src.icd10.dictionaries import ICD_10_CM
 
 
 # Form - Add, Edit Consult
@@ -37,3 +38,29 @@ class ConsultForm(FlaskForm):
     save_as_draft = SubmitField('Save as Draft', render_kw={
                                 'class': 'button-secondary'})
     cancel = SubmitField('Cancel', render_kw={'formnovalidate': True})
+
+
+class ConsultResponseForm(FlaskForm):
+    """Form to add or edit a consult response"""
+
+    comments = TextAreaField('Comments*', validators=[DataRequired()],
+                             render_kw={'class': 'u-full-width'})
+    treatment_options = TextAreaField('Treatment Options',
+                                      render_kw={'class': 'u-full-width'})
+    potential_diagnosis_1 = SelectField(
+        'Potential Diagnosis 1', coerce=int,
+        choices=ICD_10_CM,
+        render_kw={'class': 'u-full-width select2'})
+    potential_diagnosis_2 = SelectField(
+        'Potential Diagnosis 2', coerce=int,
+        choices=ICD_10_CM,
+        render_kw={'class': 'u-full-width select2'})
+    potential_diagnosis_3 = SelectField(
+        'Potential Diagnosis 3', coerce=int,
+        choices=ICD_10_CM,
+        render_kw={'class': 'u-full-width select2'})
+    potential_diagnosis_4 = SelectField(
+        'Potential Diagnosis 4', coerce=int,
+        choices=ICD_10_CM,
+        render_kw={'class': 'u-full-width select2'})
+    submit = SubmitField('Submit', render_kw={'class': 'button-primary'})
