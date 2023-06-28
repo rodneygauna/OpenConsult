8b58ae323bf77e3e459e5d7a6c457df3f2390350
Rodney Gauna <rodneygauna@gmail.com>
2023-06-28 21:27:01 +0000

    lint fix

--- src/icd10/conversion_tool.py
+++ src/icd10/conversion_tool.py
@@ -1,4 +1,4 @@
-'''
+"""
 This script will open the library of ICD-10-CM codes and descriptions and turn
 them into a list of tuples. A Python file will be created with the list of
 ICD 10 CM codes and descriptions.
@@ -7,7 +7,7 @@ The library can be found here: https://www.cms.gov/Medicare/Coding/ICD10
 
 The list of tuples will be used to populate the ICD-10-CM code and description
 for the application.
-'''
+"""
 
 
 # Imports
