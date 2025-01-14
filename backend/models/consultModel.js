e501ff0e01af7dc6d85b550fc55d34274f51ef9a
Rodney Gauna <rodneygauna@gmail.com>
2025-01-13 16:09:10 -0800

    add consult model with validation for consult details and references

---
+++ backend/models/consultModel.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+import {
+  requiredStringMaxLength,
+  requiredString,
+  optionalString,
+  requiredEnum,
+  requireRef,
+} from "../utils/validation/validationConstants.js";
+
+const consultSchema = new mongoose.Schema(
+  {
+    // Consult Status
+    status: requiredEnum("Status", [
+      "Draft", // Not yet submitted
+      "Ready for Review", // Submitted and ready for review by a specialist
+      "In Review with Specialist", // Being reviewed by a specialist
+      "Treatment Options Available", // Specialist has provided treatment options
+      "Additional Information Needed", // Specialist needs more information
+      "Different Specialty Recommended", // Specialist recommends a different specialty
+      "In Person Visit Recommended", // Specialist recommends an in person visit
+      "Cancelled", // Consult was cancelled by the Primary Care Provider
+      "Closed", // Primary Care Provider has reviewed and accepted the consult
+    ]),
+    // Consult Priority
+    priority: requiredEnum("Priority", [
+      "Routine", // Routine consult
+      "Urgent", // Urgent consult
+      "Emergent", // Emergent consult
+    ]),
+    // Consult Information
+    specialty: requiredStringMaxLength("Specialty", 255),
+    chief_complaint: requiredString("Chief complaint"),
+    comments_to_specialist: optionalString(),
+    main_question: requiredString("Main question"),
+    // Patient Reference
+    patient_id: requireRef("Patient"),
+    // Practice Reference
+    practice_id: requireRef("Practice"),
+    // Creating Provider Reference
+    creating_provider_id: requireRef("User"),
+    // Assigned Specialist Reference
+    assigned_specialist_id: requireRef("User"),
+  },
+  { timestamps: true }
+);
+
+const Consult = mongoose.model("Consult", consultSchema);
