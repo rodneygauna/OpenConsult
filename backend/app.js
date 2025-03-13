82ddd98a9e7907095c346aa17d6504115b594ad7
Rodney Gauna <rodneygauna@gmail.com>
2025-03-13 16:57:34 -0700

    feat: add routes for practices, patients, and consults in app.js

--- backend/app.js
+++ backend/app.js
@@ -11,12 +11,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// Routes - Users
-import userRoutes from "./routes/userRoutes.js";
-app.use("/api/v1/users", userRoutes);
-
 // Error handling
 app.use(notFound);
 app.use(errorHandler);
+
+// Routes - Users
+import userRoutes from "./routes/userRoutes.js";
+app.use("/api/v1/users", userRoutes);
+// Routes - Practices
+import practiceRoutes from "./routes/practiceRoutes.js";
+app.use("/api/v1/practices", practiceRoutes);
+// Routes - Patients
+import patientRoutes from "./routes/patientRoutes.js";
+app.use("/api/v1/patients", patientRoutes);
+// Routes - Consults
+import consultRoutes from "./routes/consultRoutes.js";
+app.use("/api/v1/consults", consultRoutes);
 
 export default app;
