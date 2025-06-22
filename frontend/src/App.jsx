ccb57b9574f49fc6628304680bf9546b7ce945a6
Rodney Gauna <rodneygauna@gmail.com>
2025-06-22 10:13:53 -0700

    feat: implement routing for HomePage in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,16 @@
-function App() {
-  return <></>;
-}
+import { Route, Routes } from "react-router";
+
+// Import Pages
+import HomePage from "./pages/HomePage";
+
+// App
+const App = () => {
+  return (
+    <Routes>
+      {/* HomePage Route */}
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+  );
+};
 
 export default App;
