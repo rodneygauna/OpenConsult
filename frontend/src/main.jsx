fabee7f2df8b8956ab5991bca2881ae735fb672a
Rodney Gauna <rodneygauna@gmail.com>
2025-06-22 10:13:48 -0700

    feat: wrap App component with BrowserRouter and add Toaster for notifications

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import { Toaster } from "react-hot-toast";
 import App from "./App.jsx";
 
 // Import Bootstrap CSS
@@ -7,6 +9,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+      <Toaster />
+    </BrowserRouter>
   </StrictMode>
 );
