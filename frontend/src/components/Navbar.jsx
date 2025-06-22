9f374f24112069fa2bb67df9004085ab9675a6ac
Rodney Gauna <rodneygauna@gmail.com>
2025-06-22 10:13:27 -0700

    feat: add Navbar component for site navigation

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router";
+
+// Import Bootstrap Components
+import { Navbar, Nav } from "react-bootstrap";
+
+// Navbar Component
+const NavbarComponent = () => {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand as={Link} to="/">
+        OpenConsult
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link as={Link} to="/">
+            Home
+          </Nav.Link>
+          <Nav.Link as={Link} to="/about">
+            About
+          </Nav.Link>
+          <Nav.Link as={Link} to="/contact">
+            Contact
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+// Export Navbar Component
+export default NavbarComponent;
