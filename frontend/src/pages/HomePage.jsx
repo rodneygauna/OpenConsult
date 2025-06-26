5ee7b0080de4c0458ad3c604b9bc4a697c7c2534
Rodney Gauna <rodneygauna@gmail.com>
2025-06-25 17:06:06 -0700

    test: making sure the input component works

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 // Import Components
 import NavbarComponent from "../components/Navbar.jsx";
+import InputComponent from "../components/Input.jsx";
 
 // HomePage Component
 const HomePage = () => {
@@ -9,6 +10,41 @@ const HomePage = () => {
       <div className="container">
         <h1>Welcome to MyApp</h1>
         <p>This is the home page of your application.</p>
+        <InputComponent
+          label="Search"
+          inputType="text"
+          placeholder="Type to search..."
+          subLabel="Enter keywords to find results"
+        />
+        <InputComponent
+          label="Email"
+          inputType="email"
+          placeholder="Enter your email"
+          subLabel="We'll never share your email with anyone else."
+        />
+        <InputComponent
+          label="Password"
+          inputType="password"
+          placeholder="Enter your password"
+          subLabel="Make sure it's at least 8 characters long."
+        />
+        <InputComponent
+          label="Date of Birth"
+          inputType="date"
+          placeholder="Select your date of birth"
+          subLabel="We use this to personalize your experience."
+        />
+        <InputComponent
+          label="Phone Number"
+          inputType="tel"
+          placeholder="Enter your phone number"
+          subLabel="Optional, but helps us to contact you."
+        />
+        <InputComponent
+          label="Website"
+          inputType="url"
+          placeholder="Enter your website URL"
+        />
       </div>
     </div>
   );
