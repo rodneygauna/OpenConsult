1f38fb969fb37d4483e528e4d0c0760c97f08ace
Rodney Gauna <rodneygauna@gmail.com>
2025-07-27 16:04:23 -0700

    feat: adding seed info for practices, users, and patient

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,20 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Practices
+primaryPractice1 = Practice.create!(name: "Primary Health Practice", city: "San Diego", state: "CA", zip: "92121")
+specialPractice1 = Practice.create!(name: "Specialist Practice", city: "San Diego", state: "CA", zip: "92121")
+
+# Users
+primaryUser1 = User.create!(
+  first_name: "Alex", last_name: "Smith", role: :user,
+  email_address: "alex@example.com", password: "password", practice: primaryPractice1
+)
+
+specialistUser1 = User.create!(
+  first_name: "Jordan", last_name: "Lee", role: :user,
+  email_address: "jordan@example.com", password: "password", practice: specialPractice1
+)
+
+Patient.create!(first_name: "Sam", last_name: "Jones", practice: primaryPractice1, date_of_birth: "1980-05-01")
